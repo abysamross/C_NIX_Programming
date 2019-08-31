6b8380ad06965039b34f7787eb64ac8831310881
Aby Sam Ross <abysamross@cse.iitb.ac.in>
2019-08-31 12:25:55 +0530

    Making sure expandtab is taking effect.

--- binary_trees/mbst.h
+++ binary_trees/mbst.h
@@ -7,28 +7,28 @@
 
 typedef struct bstNode {
 
-	int key;
-	void* val;
-	int height;
-	tree_root treeNode;
+    int key;
+    void* val;
+    int height;
+    tree_root treeNode;
 
 } bstNode;
 
 typedef struct bst {
 
-	tree_root root;
-	void (*insertT) (struct bst*, int, void*);
-	void* (*searchT) (struct bst*, int);
-	void (*delT) (struct bst*, int);
+    tree_root root;
+    void (*insertT) (struct bst*, int, void*);
+    void* (*searchT) (struct bst*, int);
+    void (*delT) (struct bst*, int);
 
 } bst;
 
 
-#define delete_node(root) do {								\
-			tree_del(root);									\
-			free(tree_node(root, bstNode, treeNode)->val);	\
-			free(tree_node(root, bstNode, treeNode));		\
-		} while (0)
+#define delete_node(root) do {                              \
+            tree_del(root);                                 \
+            free(tree_node(root, bstNode, treeNode)->val);  \
+            free(tree_node(root, bstNode, treeNode));       \
+        } while (0)
 
 bst* initBST(void);
 void destroyBST(bst*);
