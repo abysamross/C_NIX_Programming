3bee0c0cae1bc5c13980d55023cb20195a65247c
Aby Sam Ross <abysamross@cse.iitb.ac.in>
2019-08-31 12:34:31 +0530

    Applying the new expandtab setting

--- stacks/mstack.h
+++ stacks/mstack.h
@@ -7,50 +7,50 @@
 #define typeof(T) __typeof__(T)
 #endif
 
-#define push(S, T, D) do {							\
-				typeof(stack*) s  = S;				\
-				typeof(T) a   = D;					\
-				typeof(T)* ap = malloc(sizeof(T));	\
-				*ap = a;							\
-				s->push1(s, (void*)ap);				\
-			} while (0)
+#define push(S, T, D) do {                          \
+                typeof(stack*) s  = S;              \
+                typeof(T) a   = D;                  \
+                typeof(T)* ap = malloc(sizeof(T));  \
+                *ap = a;                            \
+                s->push1(s, (void*)ap);             \
+            } while (0)
 
-#define pop(S) ({				\
-		typeof(stack*) s  = S;	\
-		s->pop1(s);})
+#define pop(S) ({               \
+        typeof(stack*) s  = S;  \
+        s->pop1(s);})
 
-#define top(S) ({				\
-		typeof(stack*) s  = S;	\
-		s->top1(s);})
+#define top(S) ({               \
+        typeof(stack*) s  = S;  \
+        s->top1(s);})
 
-#define size(S) ({				\
-		typeof(stack*) s  = S;	\
-		s->size1(s);})
+#define size(S) ({              \
+        typeof(stack*) s  = S;  \
+        s->size1(s);})
 
-#define empty(S) ({				\
-		typeof(stack*) s  = S;	\
-		s->empty1(s);})
+#define empty(S) ({             \
+        typeof(stack*) s  = S;  \
+        s->empty1(s);})
 
 typedef struct stElem {
 
-	void* val;
-	struct stElem* nx;
-	struct stElem* pv;
+    void* val;
+    struct stElem* nx;
+    struct stElem* pv;
 
 } stElem;
 
 typedef struct stack {
 
-	const char* type;
-	void* val;
-	stElem* sp;
-	stElem* bp;
-	int nsize;
-	int (*empty1) (struct stack*);
-	void* (*top1) (struct stack*);
-	void (*pop1) (struct stack*);
-	void (*push1) (struct stack*, void*);
-	int (*size1) (struct stack*);
+    const char* type;
+    void* val;
+    stElem* sp;
+    stElem* bp;
+    int nsize;
+    int (*empty1) (struct stack*);
+    void* (*top1) (struct stack*);
+    void (*pop1) (struct stack*);
+    void (*push1) (struct stack*, void*);
+    int (*size1) (struct stack*);
 
 } stack;
 
