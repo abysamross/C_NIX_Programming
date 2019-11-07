ca69ad4ce07e02a159f29aa09df794c80570f147
Aby Sam Ross <aross@aross-ubuntu-18.04>
2019-11-08 00:43:51 +0530

    removing the unit testing exercises for now.

--- unit_testing_illustration/erase_substring.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string.h>
-
-void erase_substring(char*, const char*);
--- unit_testing_illustration/range_intersections.h
+++
@@ -1,6 +0,0 @@
-#ifndef _RANGE_INTERSECTIONS_H_
-#define _RANGE_INTERSECTIONS_H_
-
-void getIntersection(int [], int [], int* []);
-
-#endif
